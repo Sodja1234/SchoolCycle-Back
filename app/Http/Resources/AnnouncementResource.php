a2c51d45b00bb60b3bc03ab437c0227876529121
Guerchom Kubaluka <guerchomkubaluka@gmail.com>
2025-06-02 11:50:17 +0100

    refactor : ajout de la colonne 'state' dans resource

--- app/Http/Resources/AnnouncementResource.php
+++ app/Http/Resources/AnnouncementResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,12 +15,15 @@ class AnnouncementResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+
+        Carbon::setLocale('fr');
         return [
             'id'=>$this->id,
             'title'=>$this->title,
             'description'=>$this->description,
             'operation_type'=>$this->operation_type,
             'price'=>$this->price,
+            'state'=>$this->state,
             'is_completed'=>$this->is_completed,
             'is_cancelled'=>$this->is_cancelled,
             'exchange_location_address'=>$this->exchange_location_address,
@@ -27,7 +31,8 @@ class AnnouncementResource extends JsonResource
             'exchange_location_lat'=>$this->exchange_location_lat,
             'category' => new CategoryResource($this->category),
             'photos'=>$this->photos,
-            'created_by'=> new UserResource($this->user)
+            'created_by'=> new UserResource($this->user),
+            'created_at' => Carbon::parse($this->created_at)->diffForHumans()
         ];
     }
 }
