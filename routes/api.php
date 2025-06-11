ddd888bad62c635915510373c85a3409a1bec4b8
alainkatayi <alainkatayi01@gmail.com>
2025-06-11 14:51:23 +0200

    Sortie de la route categories dans le middleware

--- routes/api.php
+++ routes/api.php
@@ -31,11 +31,13 @@ Route::middleware('auth:sanctum')->group(function () {
 
 // Routes publiques
 Route::apiResource('/announcements', AnnouncementController::class)->only(['index', 'show']);
+Route::apiResource('/categories', CategoryController::class)->only(['index', 'show']);
+
 
 // Routes protégées
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('/announcements', AnnouncementController::class)->except(['index', 'show']);
-    Route::apiResource('/categories', CategoryController::class);
+    Route::apiResource('/categories', CategoryController::class)->except(['index', 'show']);
 });
 
 
