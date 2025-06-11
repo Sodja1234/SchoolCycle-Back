c52172061529f38d77a76341c85fbde0aa8cf2e7
alainkatayi <alainkatayi01@gmail.com>
2025-06-11 18:33:43 +0200

    Ajout de plusieurs catégories prédéfinies dans le seeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
 
 class CategorySeeder extends Seeder
 {
@@ -13,6 +14,61 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        Category::factory()->count(10)->create();
+        $faker = Faker::create();
+        $categories = [
+            'stylo',
+            'Crayon à papier',
+            'Porte-mine',
+            'Gomme',
+            'Taille-crayon',
+            'Correcteur',
+            'Marqueur',
+            'Stabilo (surligneur)',
+            'Feutres de couleurs',
+            'Cahiers ',
+            'Bloc-notes',
+            'Fiches bristol',
+            'Feuilles simples',
+            'Copies doubles',
+            'Papier millimétré',
+            'Carnet de brouillon',
+            'Carnet de vocabulaire',
+            'Règle',
+            'Équerre',
+            'Rapporteur',
+            'Compas',
+            'Calculatrice simple',
+            'Calculatrice scientifique',
+            'Ardoise ',
+            'feutre ',
+            'Crayons de couleur',
+            'Pastels',
+            'Peinture',
+            'pinceaux',
+            'Gobelet pour eau',
+            'Tabliers',
+            'Ciseaux',
+            'Colle',
+            'Papier de couleur',
+            'Papier Canson',
+            'Trousse',
+            'Cartable / Sac à dos',
+            'Pochette plastique perforée',
+            'Classeur',
+            'Intercalaires',
+            'Chemise à rabats',
+            'Porte-documents',
+            'Agrafeuse',
+            'Scotch / ruban adhésif',
+            'Attaches parisiennes',
+            'Mouchoirs',
+            'Bouteille d’eau'
+        ];
+        foreach ($categories as $category) {
+            Category::create([
+                "name" => $category,
+                "description" => $faker->sentence(10),
+            ]);
+        }
     }
 }
