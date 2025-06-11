f5bf6c7eb1194956973fb279fbc029069920ef52
alainkatayi <alainkatayi01@gmail.com>
2025-06-11 15:04:57 +0200

    Nouvelle Ajout du champ state dans la table announcement

---
+++ database/migrations/2025_06_11_125338_add_state_in_announcement_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+       public function up(): void
+    {
+        Schema::table('announcements', function (Blueprint $table) {
+            $table->enum('state',['new','good','damaged','like new']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('announcements', function (Blueprint $table) {
+            $table->dropColumn('state');
+        });
+    }
+};
