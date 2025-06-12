6bd0244d1a1c4f19f8644588ea91194c8abec8a1
29delia <155080499+29delia@users.noreply.github.com>
2025-06-12 13:52:55 +0100

    ajout de la resource TutorResource

---
+++ app/Http/Resources/TutorResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Resources\UserResource;
+
+class TutorResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'user' => new UserResource($this->user),
+            'telephone' => $this->telephone,
+            'profession' => $this->profession,
+            'adresse' => $this->adresse,
+            'bio' => $this->bio,
+            'avatar' => $this->avatar ? asset('storage/' . $this->avatar) : null,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+   
+}
