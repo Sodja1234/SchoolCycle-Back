548bf310ad913fdf173e46fa759709f9a231c8f6
0904STANISLAS <osangostanislas@gmail.com>
2025-06-26 10:52:15 +0100

    Ajout de la ressource MessageRessource pour la transformation des messages en JSON : envoie des informations détaillées sur l'expéditeur et le destinataire, et formatage de la date de création en heure.

---
+++ app/Http/Resources/MessageRessource.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class MessageRessource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        Carbon::setLocale('fr');
+        return [
+            'id' => $this->id,
+            'conversation' => $this->conversation,
+            'sender' => [
+                'id' => $this->sender,
+                'name' => optional($this->senderUser)->name
+            ],
+            'receiver' => [
+                'id' => $this->receiver,
+                "name" => optional($this->receiverUser)->name
+            ],
+            'content' => $this->content,
+            'created_at' => Carbon::parse($this->created_at)->format('H:i'),
+        ];
+    }
+}
