8dea86342223c5abeb4834ec8be51142c6fa8ba4
alainkatayi <alainkatayi01@gmail.com>
2025-07-21 12:51:49 +0200

    Ajout de swagger dans le model Announcement

--- app/Models/Announcement.php
+++ app/Models/Announcement.php
@@ -8,6 +8,35 @@ use App\Models\Category;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+
+/**
+ * @OA\Schema(
+ *     schema="Announcement",
+ *     type="object",
+ *     title="Annonce",
+ *     @OA\Property(property="id", type="integer", example=1),
+ *     @OA\Property(property="title", type="string", example="Sac à dos blanc"),
+ *     @OA\Property(property="description", type="string", example="Sac à dos blanc pour ecolier, avec trois poche "),
+ *     @OA\Property(property="price", type="number", format="float", example=750.50),
+ *     @OA\Property(property="operation type", type="string",  example="evente"),
+ *     @OA\Property(property="state", type="string",  example="new"),
+ *     @OA\Property(property="category", type="object",
+ *         @OA\Property(property="id", type="integer", example=3),
+ *         @OA\Property(property="name", type="string", example="Immobilier")
+ *     ),
+ *     @OA\Property(property="photos", type="array",
+ *         @OA\Items(
+ *             type="object",
+ *             @OA\Property(property="url", type="string", example="http://example.com/photo.jpg")
+ *         )
+ *     ),
+ *     @OA\Property(property="user", type="object",
+ *         @OA\Property(property="id", type="integer", example=4),
+ *         @OA\Property(property="name", type="string", example="Alain Katayi")
+ *     )
+ * )
+ */
+
 class Announcement extends Model
 {
     use HasFactory;
