e4cf5d1559c61aa75371078488b6722fbd40c75a
alainkatayi <alainkatayi01@gmail.com>
2025-08-05 18:45:14 +0200

    Ajout de swagger dans le fichier controller pour avoir toutes les annotations des autres controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,22 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * @OA\Info(
+ *     title="SchoolCycle API",
+ *     version="1.0.0",
+ *     description="Documentation de l'API de SchoolCycle",
+ *     @OA\Contact(
+ *         email="support@schoolcycle.com",
+ *         name="Support Team"
+ *     )
+ * )
+ *
+ * @OA\Server(
+ *     url=L5_SWAGGER_CONST_HOST,
+ *     description="Serveur principal"
+ * )
+ */
 abstract class Controller
 {
     //
